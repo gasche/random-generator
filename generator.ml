6781a32d05971a8901f121304f442c6726b56700
Gabriel Scherer <gabriel.scherer@gmail.com>
2013-10-25 16:01:59 +0200

    relicense under BSD
    Simon Cruanes expressed his interest in incorporating bits of
    random-generator in his QCheck library
    ( https://github.com/c-cube/qcheck/ ), but he's too afraid of
    hereditary licenses to properly reuse GPL work. I'm relicensing this
    under BSD (after checking with Xavier) to make everyone's life easier.

--- generator.ml
+++ generator.ml
@@ -3,19 +3,29 @@
  * Copyright (C) 2008-2012 Xavier Clerc
  *               2013      Gabriel Scherer
  *
- * This library evolved from the Generator module of Xavier Clerc's
+ * This library evolved from experiments on the Generator module of Xavier Clerc's
  * Kaputt library: http://kaputt.x9c.fr/
  *
- * This library is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License <http://www.gnu.org/licences> for more
- * details.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * - Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the
+ * distribution.
+ * 
+ * This software is provided by the copyright holders and contributors "as is" and
+ * any express or implied warranties, including, but not limited to, the implied
+ * warranties of merchantability and fitness for a particular purpose are
+ * disclaimed. In no event shall the copyright holder or contributors be liable
+ * for any direct, indirect, incidental, special, exemplary, or consequential
+ * damages (including, but not limited to, procurement of substitute goods or
+ * services; loss of use, data, or profits; or business interruption) however
+ * caused and on any theory of liability, whether in contract, strict liability,
+ * or tort (including negligence or otherwise) arising in any way out of the use
+ * of this software, even if advised of the possibility of such damage.
  *)
 
 type random_state = Random.State.t
